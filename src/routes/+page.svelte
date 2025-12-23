d004a23ee2b07ff115ffb07c6c8f07f396764205
Egor Ulybin <egorulybin@gmail.com>
2025-12-23 17:02:42 +0100

    for each loop implemented for cart and products

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -34,7 +34,12 @@
 		<section>
 			<h2>Available Products</h2>
 			<div>
-				<span>for each here</span>
+				{#each products as product}
+					<button onclick={() => addProduct(product)}>
+						<span>{product.name}</span>
+						<span>€{product.price.toFixed(2)}</span>
+					</button>
+				{/each}
 			</div>
 		</section>
 
@@ -42,11 +47,29 @@
 		<section>
 			<h2>Shopping Cart</h2>
 			<div>
-				<ul>Selected products</ul>
+				{#if cart.length === 0}
+					<p>No products in the cart</p>
+				{:else}
+					<ul>
+						{#each cart as item (item.id)}
+							<li>
+								<div>
+									<span>{item.name}</span>
+									<span>€{item.price.toFixed(2)}</span>
+									{#if item.quantity > 1}
+										<span>x{item.quantity}</span>
+									{/if}
+								</div>
+								<button onclick={() => removeProduct(item.id)}> Remove </button>
+							</li>
+						{/each}
+					</ul>
+				{/if}
 			</div>
 			<div>
 				<div>
 					<span>Total:</span>
+					<span>${total.toFixed(2)}</span>
 				</div>
 			</div>
 		</section>
